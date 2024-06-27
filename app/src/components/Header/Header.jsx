474c5727f4e4cd5ecc52ee14393208167232e460
Rafael Teodosio <rafzteodosio@gmail.com>
2024-06-26 22:01:58 -0300

    Create Footer, Header, Home

---
+++ app/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{backgroundColor: 'rgb(237 119 0)'}} className="text-white text-center py-3">
+      <h1>Konatus Interface</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ app/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mt-4">
+      <h2>Welcome to the API Consumer Interface</h2>
+      <p>Select an API from the navigation bar to see the data.</p>
+    </div>
+  );
+};
+
+export default Home;
