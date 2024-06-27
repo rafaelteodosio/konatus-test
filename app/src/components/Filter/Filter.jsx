4a0faa0cc92fe87b1cd779de9d801b6854b909f6
Rafael Teodosio <rafzteodosio@gmail.com>
2024-06-26 22:01:34 -0300

    Create Filter Component

---
+++ app/src/components/Filter/Filter.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { InputGroup, Form, FormControl } from 'react-bootstrap';
+
+const Filter = ({ onFilterChange, options = [] }) => {
+  const [selectedOption, setSelectedOption] = useState('');
+  
+  const handleOptionChange = (e) => {
+    const option = e.target.value;
+    setSelectedOption(option);
+    onFilterChange(option);
+  };
+
+  return (
+    <div className="mb-3">
+      <InputGroup>
+        <InputGroup.Text>Filtro</InputGroup.Text>
+        <Form.Select value={selectedOption} onChange={handleOptionChange}>
+          {options.map((option, index) => (
+            <option key={index} value={option}>{option}</option>
+          ))}
+        </Form.Select>
+      </InputGroup>
+    </div>
+  );
+};
+
+export default Filter;
