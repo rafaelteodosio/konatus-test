46b5e4f225704d5d968634933411109b520db1fb
Rafael Teodosio <rafzteodosio@gmail.com>
2024-06-26 21:58:38 -0300

    Add Routes to App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import NavigationBar from './components/NavigationBar/NavigationBar';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import ApiPage from './api/ApiPage';
+import MakeupTable from './components/Table/MakeupTable';
+import CardsTable from './components/Table/CardsTable';
+import JSONPlaceholderTable from './components/Table/JSONPlaceholderTable';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/jsonplaceholder"
+          element={
+            <ApiPage
+              Content={JSONPlaceholderTable}
+            />}
+        />
+        <Route
+          path="/makeup"
+          element={
+            <ApiPage
+              Content={MakeupTable}
+            />
+          }
+        />
+        <Route
+          path="/deckofcards"
+          element={<ApiPage
+            Content={CardsTable}
+          />}
+        />
+      </Routes>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
